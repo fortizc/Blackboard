40f1c15969ed04bf6039c55d830913735afc2e7f
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-17 11:30:00 -0400

    Add a label list init

--- blackboard/gui/result_pane.py
+++ blackboard/gui/result_pane.py
@@ -3,12 +3,19 @@ gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk, Gio  # noqa
 
+    def init_lb_list(self):
+        lb = Gtk.Label(str(0))
+        self.lb_list = []
+        self.lb_list.append(lb)
+        self.add(lb)
+        lb.show()
 
 class result_pane(Gtk.Box):
     def __init__(self):
         Gtk.Box.__init__(self)
         self.orientation = Gtk.Orientation.VERTICAL
         self.spacing = 0
+        self.init_lb_list()
 
     def add_label(self, txt_op, y):
         x = txt_op.get_cursor_locations()
