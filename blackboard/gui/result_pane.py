3b34481a16c0414f71f3d954a5742ffa03efd06b
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-17 11:33:59 -0400

    Add a new label only when a 'Return' key is pressed

--- blackboard/gui/result_pane.py
+++ blackboard/gui/result_pane.py
@@ -1,14 +1,8 @@
 import gi
 gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gtk, Gio  # noqa
+from gi.repository import Gtk, Gio, Gdk  # noqa
 
-    def init_lb_list(self):
-        lb = Gtk.Label(str(0))
-        self.lb_list = []
-        self.lb_list.append(lb)
-        self.add(lb)
-        lb.show()
 
 class result_pane(Gtk.Box):
     def __init__(self):
@@ -17,8 +11,12 @@ class result_pane(Gtk.Box):
         self.spacing = 0
         self.init_lb_list()
 
-    def add_label(self, txt_op, y):
-        x = txt_op.get_cursor_locations()
-        lb = Gtk.Label(str(x.strong.y))
-        self.add(lb)
-        lb.show()
+
+    def add_label(self, txt_op, value):
+        if Gdk.keyval_name(value.keyval) == 'Return':
+            x = txt_op.get_cursor_locations()
+            pos = x.strong.y / x.strong.height + 1
+            lb = Gtk.Label(str(pos))
+            self.lb_list.insert(pos, lb)
+            self.add(lb)
+            lb.show()
