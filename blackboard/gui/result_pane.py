be87f124cecf3afa5b05f769eb9a682aa235c98f
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-17 22:10:51 -0400

    Change the Gtk.Box by Gtk.Grid to show labels correctly

--- blackboard/gui/result_pane.py
+++ blackboard/gui/result_pane.py
@@ -4,25 +4,24 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gio, Gdk  # noqa
 
 
-class result_pane(Gtk.Box):
+class result_pane(Gtk.Grid):
     def __init__(self):
-        Gtk.Box.__init__(self)
-        self.orientation = Gtk.Orientation.VERTICAL
-        self.spacing = 0
+        Gtk.Grid.__init__(self)
         self.__init_lb_list()
 
     def __init_lb_list(self):
         lb = Gtk.Label(str(0))
         self.lb_list = []
         self.lb_list.append(lb)
-        self.add(lb)
+        self.attach(lb, 0, 0, 100, 20)
         lb.show()
 
     def add_label(self, txt_op, value):
         if Gdk.keyval_name(value.keyval) == 'Return':
             x = txt_op.get_cursor_locations()
-            pos = x.strong.y / x.strong.height + 1
-            lb = Gtk.Label(str(pos))
-            self.lb_list.insert(pos, lb)
-            self.add(lb)
+            pos = x.strong.y + x.strong.height
+            index = pos / x.strong.height
+            lb = Gtk.Label(str(index))
+            self.lb_list.insert(index, lb)
+            self.attach(lb, 0, pos, 100, x.strong.height)
             lb.show()
