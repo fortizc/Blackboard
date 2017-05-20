435899b221d18940c3c4021d072594f807a9c4a7
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-19 23:21:07 -0400

    Move the result labels with the operations

--- blackboard/gui/result_pane.py
+++ blackboard/gui/result_pane.py
@@ -13,15 +13,18 @@ class result_pane(Gtk.Grid):
         lb = Gtk.Label(str(0))
         self.lb_list = []
         self.lb_list.append(lb)
-        self.attach(lb, 0, 0, 100, 20)
+        self.attach(lb, 0, 0, 1, 1)
         lb.show()
 
     def add_label(self, txt_op, value):
         if Gdk.keyval_name(value.keyval) == 'Return':
             x = txt_op.get_cursor_locations()
-            pos = x.strong.y + x.strong.height
-            index = pos / x.strong.height
+            height = x.strong.height
+            pos = x.strong.y + height
+            index = pos / height
             lb = Gtk.Label(str(index))
             self.lb_list.insert(index, lb)
-            self.attach(lb, 0, pos, 100, x.strong.height)
+            if (len(self.lb_list) - 1) > index:
+                self.insert_row(index)
+            self.attach(lb, 0, pos, 1, 1)
             lb.show()
