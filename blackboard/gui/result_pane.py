d3bd51a2b6ae4e9d234e16c2c0e3ad36a6aa3ecf
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-20 01:05:24 -0400

    Change add label as private and add capture pressed key

--- blackboard/gui/result_pane.py
+++ blackboard/gui/result_pane.py
@@ -7,24 +7,22 @@ from gi.repository import Gtk, Gio, Gdk  # noqa
 class result_pane(Gtk.Grid):
     def __init__(self):
         Gtk.Grid.__init__(self)
-        self.__init_lb_list()
-
-    def __init_lb_list(self):
-        lb = Gtk.Label(str(0))
         self.lb_list = []
-        self.lb_list.append(lb)
-        self.attach(lb, 0, 0, 1, 1)
+        self.__add_label(0)
+
+    def __add_label(self, index):
+        lb = Gtk.Label(str(index))
+        self.lb_list.insert(index, lb)
+        if (len(self.lb_list) - 1) > index:
+            self.insert_row(index)
+        self.attach(lb, 0, index, 1, 1)
         lb.show()
 
-    def add_label(self, txt_op, value):
-        if Gdk.keyval_name(value.keyval) == 'Return':
-            x = txt_op.get_cursor_locations()
-            height = x.strong.height
-            pos = x.strong.y + height
-            index = int(pos / height)
-            lb = Gtk.Label(str(index))
-            self.lb_list.insert(index, lb)
-            if (len(self.lb_list) - 1) > index:
-                self.insert_row(index)
-            self.attach(lb, 0, pos, 1, 1)
-            lb.show()
+    def capture_pressed_key(self, txt_op, value):
+        if Gdk.keyval_name(value.keyval) != 'Return':
+            return
+        x = txt_op.get_cursor_locations()
+        height = x.strong.height
+        pos = x.strong.y + height
+        index = int(pos / height)
+        self.__add_label(index)
