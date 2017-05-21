002365b7c03678aa17e6356611b34cfd598c1e74
Pablo Ahumada <pablo.ahumadadiaz@gmail.com>
2017-05-20 21:41:34 -0400

    Restructure methods and their names

--- blackboard/gui/result_pane.py
+++ blackboard/gui/result_pane.py
@@ -8,9 +8,9 @@ class result_pane(Gtk.Grid):
     def __init__(self):
         Gtk.Grid.__init__(self)
         self.lb_list = []
-        self.__add_label(0)
+        self.__add_label_at(0)
 
-    def __add_label(self, index):
+    def __add_label_at(self, index):
         lb = Gtk.Label(str(index))
         self.lb_list.insert(index, lb)
         if (len(self.lb_list) - 1) > index:
@@ -18,11 +18,14 @@ class result_pane(Gtk.Grid):
         self.attach(lb, 0, index, 1, 1)
         lb.show()
 
-    def capture_pressed_key(self, txt_op, value):
-        if Gdk.keyval_name(value.keyval) != 'Return':
-            return
+    def __get_cursor_position(self, txt_op):
         x = txt_op.get_cursor_locations()
         height = x.strong.height
         pos = x.strong.y + height
         index = int(pos / height)
-        self.__add_label(index)
+        return index
+
+    def add_label_at_cursor(self, txt_op, value):
+        if Gdk.keyval_name(value.keyval) != 'Return':
+            return
+        self.__add_label_at(self.__get_cursor_position(txt_op))
