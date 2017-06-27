2509d1925836d782fe6559d55439e22b6751c97f
Felipe Ortiz <fortizc@gmail.com>
2017-06-26 23:02:36 -0400

    Now get_keywords method retunr a list of keys instead of a dict_keys

--- blackboard/plugin/plugin_loader.py
+++ blackboard/plugin/plugin_loader.py
@@ -15,7 +15,7 @@ class _plugin_loader:
         self.__plugins = {}
         self.__load_plugins(self.__core_path)
         self.__load_plugins(self.__user_path)
-        self.__kwords = self.__plugins.keys()
+        self.__kwords = list(self.__plugins.keys())
 
     def get_plugin(self, identifier):
         return self.__plugins.get(identifier, None)
